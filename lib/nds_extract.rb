33cc60297013bc9748cf07b085842df718a7175a
Luis Pestana <lpestana@protonmail.com>
2020-02-28 22:59:04 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -59,7 +59,6 @@ end
 
 
 def gross_per_studio(collection)
-  pp 
   # GOAL: Given an Array of Hashes where each Hash represents a movie,
   # return a Hash that includes the total worldwide_gross of all the movies from
   # each studio.
