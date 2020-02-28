dc092781eb32a783a4d3ba20b50ed008048d29c3
Luis Pestana <lpestana@protonmail.com>
2020-02-28 22:59:55 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -86,6 +86,7 @@ def gross_per_studio(collection)
 end
 
 def movies_with_directors_set(source)
+  pp source
   # GOAL: For each director, find their :movies Array and stick it in a new Array
   #
   # INPUT:
